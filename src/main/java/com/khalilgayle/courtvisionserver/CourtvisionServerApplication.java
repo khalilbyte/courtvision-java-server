d3ddd8977cb6d58cc86dc27e83989f4f8e07706c
fullstackkg <khalilhgayle@gmail.com>
2024-12-14 13:42:26 -0500

    Initial commit and creation of PlayerDTO

---
+++ src/main/java/com/khalilgayle/courtvisionserver/CourtvisionServerApplication.java
@@ -0,0 +1,13 @@
+package com.khalilgayle.courtvisionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtvisionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourtvisionServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khalilgayle/courtvisionserver/CourtvisionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalilgayle.courtvisionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtvisionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
